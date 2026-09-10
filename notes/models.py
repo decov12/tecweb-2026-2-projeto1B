e4ab9c5fa7ec9df3973f1a6f0d47a409d99d27b6
André Vasconcellos <andretbv@al.insper.edu.br>
2026-09-10 09:33:58 -0300

    handout_feito

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(max_length=2000)
+    def __str__(self):
+        resposta=""
+        resposta+=str(self.id)
+        resposta+=". "
+        resposta+=self.title
+        return resposta
--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,13 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Note
 
 
 def index(request):
-    return HttpResponse("Olá mundo! Este é o app notes de Tecnologias Web do Insper.")
+    if request.method == 'POST':
+        title = request.POST.get('titulo')
+        content = request.POST.get('detalhes')
+        Note.objects.create(title=title, content=content)
+        return redirect('index')
+    else:
+        all_notes = Note.objects.all()
+        return render(request, 'notes/index.html', {'notes': all_notes})
