a081ed286f0bc32b0e2dc1ba97f9797f921114a8
André Vasconcellos <andretbv@al.insper.edu.br>
2026-09-10 09:45:34 -0300

    tarefa_01

--- notes/urls.py
+++ notes/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('notes/<int:note_id>/edit/', views.edit, name='edit'),
+    path('notes/<int:note_id>/delete/', views.delete, name='delete'),
 ]
